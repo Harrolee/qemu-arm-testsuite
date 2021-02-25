9876c8f2d24017f55c797ed6667c5c0e0c1c2cf0
= <lharrold@scu.edu>
2021-02-25 09:49:27 -0800

    file server api

--- k8s/pods/command_hub/web_app/web_server.py
+++ k8s/pods/command_hub/web_app/web_server.py
@@ -1,13 +1,33 @@
 # This will publish tests to the broker.
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse 
 import test_publisher as test_pub
+
 
 app = FastAPI()
 
+code_dir = "/fastapi/code_to_emulate/"
+filename = None
 
 @app.post("/upload-code/")
 async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    # write file to file
+    filename = file.filename
+    code = file.read()
+    return {"filename": file.filename, "filesize": code.__sizeof__()}
+
+@app.get("/get-code")
+async def serve_code():
+    return FileResponse(f'{code_dir}/{filename}')
+
+@app.get("/get-apple")
+async def serve_code():
+    return FileResponse(f'{code_dir}apple.txt')
+
+@app.get("/test")
+async def serve_code():
+    return "this works"
+
 
 # run tests on emulated devices
 @app.post("/runall")
